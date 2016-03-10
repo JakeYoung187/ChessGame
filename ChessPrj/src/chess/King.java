86f09c6ea8173a5689056bb01a6e494753b210ab
mechanicalmaniac <deusexmadmachina@gmail.com>
2016-03-09 22:12:36 -0500

    overridan isValidMove() -- not done yet
    Not sure if there's a way to avoid repeating ourselves in other overrides with the declarations of method variables for toRow, fromRow, etc.

--- ChessPrj/src/chess/King.java
+++ ChessPrj/src/chess/King.java
@@ -1,5 +1,17 @@
 package chess;
 
 public class King extends ChessPiece{
-
+  
+  public boolean isValidMove(Move move, IChessPiece[][] board) {
+    boolean vtr = true;
+    int toRow = move.toRow;
+    int fromRow = move.fromRow;
+    int toColumn = move.toColumn;
+    int fromColumn = move.fromColumn
+    
+    if (fromRow > toRow - 1 || fromRow < toRow + 2 ||
+      fromColumn > toColumn - 1 || fromColumn < toColumn + 2)
+      vtr = false;
+    return vtr;
+  }
 }
