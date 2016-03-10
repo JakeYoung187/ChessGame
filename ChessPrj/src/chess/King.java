b5acc1559721c5587b66431d47d047d070ae2251
mechanicalmaniac <deusexmadmachina@gmail.com>
2016-03-10 11:48:18 -0500

    Update King.java

--- ChessPrj/src/chess/King.java
+++ ChessPrj/src/chess/King.java
@@ -2,12 +2,26 @@ package chess;
 
 public class King extends ChessPiece{
   
+  // Think there's a way to return a string describing _why_ 
+  // the move you're trying to make is illegal? -- Henry
+  
+  /**
+   * Returns whether or not the piece is allowed to make a particular
+   * move given the current board state.
+   * 
+   * @param move the move to be made
+   * @param board the board the piece is on
+   * @return the legality of the move
+   **/
   public boolean isValidMove(Move move, IChessPiece[][] board) {
     boolean vtr = true;
     int toRow = move.toRow;
     int fromRow = move.fromRow;
     int toColumn = move.toColumn;
     int fromColumn = move.fromColumn
+    
+    if (toRow < 0 || toRow > 7 || toColumn < 0 || toColumn > 7)
+      vtr = false;
     
     if (fromRow > toRow - 1 || fromRow < toRow + 2 ||
       fromColumn > toColumn - 1 || fromColumn < toColumn + 2)
